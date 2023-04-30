88bcd55c350fc7a4975bf1e9b889042b92920d99
RudraModi360 <130299388+RudraModi360@users.noreply.github.com>
2023-04-30 12:03:28 +0530

    Add files via upload

---
+++ filters.py
@@ -0,0 +1,75 @@
+from rembg import remove
+from PIL import Image
+import numpy as np
+import cv2
+import os
+from pathlib import Path
+home = str(Path.home()) + "/Downloads/"
+
+
+def Filter_gray(img):
+    sf_name = img
+    img = cv2.imread(img, cv2.IMREAD_COLOR)
+    img = cv2.medianBlur(img, 1)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite(os.path.join(home, 'gray_' + os.path.basename(sf_name)),gray)
+
+def Edges(img):
+    sf_name=img
+    img = cv2.imread(img,cv2.IMREAD_COLOR)
+    img = cv2.medianBlur(img, 1)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+    edges_color = cv2.applyColorMap(edges, cv2.COLORMAP_WINTER)
+    cv2.imwrite(os.path.join(home, 'Edged_'+os.path.basename(sf_name)),edges_color)
+
+# def merge(img):
+#     sf_name=img
+#     img = Image.open(img)
+#     output = remove(img)
+#     # output.save("removed_BG.png")
+#     cv2.imwrite(os.path.join(home, os.path.basename(sf_name)),output)
+#     mi1 = Image.open("removed_BG.png")
+#     mi2 = Image.open("bg.png")
+#     mi2.paste(mi1, (0, 0), mi1)
+#     mi2.save("merged_image.png")
+
+def removebg(img):
+    sf_name=img
+    img = Image.open(img)
+    output = remove(img)
+    # output.save("removed_BG.png")
+    output.save(os.path.join(home, 'Removed_BG_'+ os.path.basename(sf_name)[0:-3] + 'png'))
+
+
+def cartoon(img):
+    sf_name=img
+    img = cv2.imread(img, cv2.IMREAD_COLOR)
+    img = cv2.medianBlur(img, 1)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+    # edges_color = cv2.applyColorMap(edges, cv2.COLORMAP_JET)
+    color = cv2.bilateralFilter(img,1, 250, 250)
+    cartoon = cv2.bitwise_and(color, color, mask=edges)
+    cv2.imwrite(os.path.join(home, 'cartoon_'+os.path.basename(sf_name)),cartoon)
+def cartoonize(img, k):
+    sf_name=img
+    img = cv2.imread(img, cv2.IMREAD_COLOR)
+    data = np.float32(img).reshape((-1, 3))
+    print("shape of input data: ", img.shape)
+    print("shape of resized data", data.shape)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 20, 1.0)
+    _, label, center = cv2.kmeans(
+        data, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS
+    )
+    center = np.uint8(center)
+    print(center)
+    result = center[label.flatten()]
+    result = result.reshape(img.shape)
+    # cv2.imshow("result", result)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    edges  = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 9, 8)
+    blurred = cv2.medianBlur(result, 3)
+    cartoon = cv2.bitwise_and(blurred, blurred, mask=edges)
+    cv2.imwrite(os.path.join(home, 'anime_'+os.path.basename(sf_name)),cartoon)
+
---
+++ toonifygui.py
@@ -0,0 +1,98 @@
+import filters
+import flet
+from flet import (
+    ElevatedButton,
+    FilePicker,
+    FilePickerResultEvent,
+    Page,
+    Row,
+    Text,
+    icons,
+)
+
+
+def main(page: Page):
+
+    img_url = 'C:\\Users\\havit\\OneDrive\\Pictures\\Camera Roll\\WIN_20230429_14_32_53_Pro.jpg'
+    img = flet.Image(
+        src=str(img_url),
+        width=600,
+        height=400,
+        fit=flet.ImageFit.CONTAIN,
+    )
+    images = flet.Row(expand=1, wrap=False, scroll="always")
+    page.update()
+    page.add(img, images)
+    # Pick files dialog
+    file_path = []
+    def pick_files_result(e: FilePickerResultEvent):
+        global img_url
+        selected_files.value = (
+            ", ".join(map(lambda f: f.path, e.files)) if e.files else "Cancelled!"
+        )
+        path = e.path if e.path else "Cancelled!"
+
+        file_path.insert(0, selected_files.value)
+        img.src = file_path[0]
+        page.update()
+        print(file_path)
+        selected_files.update()
+        page.update()
+
+
+
+    pick_files_dialog = FilePicker(on_result=pick_files_result)
+    selected_files = Text()
+
+
+    # Save file dialog
+    def save_file_result(e: FilePickerResultEvent):
+        filters.Filter_gray(file_path[0])
+        save_file_path.value = e.path if e.path else "Cancelled!"
+        print(save_file_path)
+        save_file_path.update()
+
+    save_file_dialog = FilePicker(on_result=save_file_result)
+    save_file_path = Text()
+
+    def bw(request):
+        filters.Filter_gray(file_path[0])
+    def cartoon(request):
+        filters.cartoon(file_path[0])
+    def anime(request):
+        filters.cartoonize(file_path[0], 8)
+    def removebg(request):
+        filters.removebg(file_path[0])
+    def sketch(request):
+        filters.Edges(file_path[0])
+    # hide all dialogs in overlay
+    page.overlay.extend([pick_files_dialog, save_file_dialog])
+
+    page.add(
+        Row(
+            [
+                ElevatedButton(
+                    "Pick files",
+                    icon=icons.UPLOAD_FILE,
+                    on_click=lambda _: pick_files_dialog.pick_files(
+                        allow_multiple=False
+                    ),
+                ),
+                selected_files,
+            ]
+        ),
+
+        Row(
+            [
+                ElevatedButton("Anime Effect", on_click=anime),
+                ElevatedButton("Sckech Effect", on_click=sketch),
+                ElevatedButton("Black/White", on_click=bw),
+                ElevatedButton("Cartoon Effect", on_click=cartoon),
+                ElevatedButton("Remove Background", on_click=removebg)
+            ]
+        )
+
+    )
+
+
+flet.app(target=main)
